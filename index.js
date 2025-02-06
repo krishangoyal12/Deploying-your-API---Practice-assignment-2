0301ba6f9b7cdc6850087342fad9fd3bfff487eb
krishangoyal12 <krishangoyal717@gmail.com>
2025-02-06 11:21:19 +0530

    environment variables

--- index.js
+++ index.js
@@ -6,9 +6,42 @@ const port = 3010;
 
 app.use(express.static('static'));
 
+require('dotenv').config()
+
+const isAdmin = process.env.IS_ADMIN === 'true';
+
+if (isAdmin) {
+  console.log("Admin privileges granted.");
+} else {
+  console.log("Access restricted. Admin only.");
+}
+
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+
+
+app.get('/login', (req, res) => {
+  if (process.env.IS_ADMIN === 'true') {
+    res.send({ 
+
+      message: "Welcome, Admin!", 
+      data: ["Krishan Goyal", "John Doe"] 
+    
+    });
+
+  } 
+  
+  else {
+    res.send({ 
+
+      message: "Welcome, User!",
+      data: ["User1", "User2"] 
+    
+    });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
